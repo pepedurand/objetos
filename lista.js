5481bea2844a2abc907ee01d32bc3db8cbd6aff8
Pepe <webpedrod@gmail.com>
2022-03-09 22:42:47 -0300

    Feat: arquivos novos

---
+++ lista.js
@@ -0,0 +1,30 @@
+const clientes = [
+    {
+    nome: 'Pedro',
+    idade: 25,
+    cpf: '111111111',
+    email: 'webpedro@gmail.com',
+    fones: "3333333",
+    dependentes: [{
+        nome: 'Sara',
+        parentesco: 'irma',
+        idade: 15,
+        }]
+},
+    {
+    nome: 'Jaq',
+    idade: 26,
+    cpf: '2222222',
+    email: 'jaq@gmail.com',
+    fones: "4444444",
+    dependentes: [{
+        nome: 'carol',
+        parentesco: 'irma',
+        idade: 13,
+        }]
+    }
+]
+
+const listaDepedentes = [...clientes[0].dependentes, ...clientes[1].dependentes]
+
+console.log (listaDepedentes)
